d2cb3b69dbedba67adee2c7fb19b910a4f4805a6
Arsal Naeem <arsalnaeem1@gmail.com>
2025-02-10 05:24:59 +0500

    Refactor CategoryBreakdown to fetch data from API and remove hardcoded categories

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,14 @@
-'use client'
+"use client";
 import CategoryBreakdown from "@/components/Dashboard/CategoryBreakdown/CategoryBreakdown";
 import MonthlyBalanceTrend from "@/components/Dashboard/MonthlyBalanceTrend/MonthlyBalanceTrend";
 import Stats from "@/components/Dashboard/Stats/Stats";
 import MainLayout from "@/components/MainLayout/MainLayout";
-import TransactionModal from "@/components/Modals/TransactionModal/TransactionModal";
 import TransactionsTable from "@/components/TransactionTable/TransactionTable";
 import { useSession } from "next-auth/react";
 import { Col, Row } from "antd";
 
 export default function Dashboard() {
   const { data: session } = useSession();
-  const categoryData = [
-    { category: "Groceries", amount: 500, color: "#a2f345" },
-    { category: "Utilities", amount: 300, color: "#458bf3" },
-    { category: "Entertainment", amount: 200, color: "#f3d345" },
-    { category: "Transport", amount: 150, color: "#FF8042" },
-    { category: "Transpsdort", amount: 50, color: "#7c45f3" },
-  ];
 
   const stats = {
     currentBalance: 5000,
@@ -30,7 +22,7 @@ export default function Dashboard() {
       <div
         style={{ padding: "24px 16px 8px 16px ", color: "var(--color-accent)" }}
       >
-        <h2>Hi, {session?.user?.name || 'Guest'}</h2>
+        <h2>Hi, {session?.user?.name || "Guest"}</h2>
       </div>
       <Stats
         currentBalance={stats?.currentBalance}
@@ -44,7 +36,7 @@ export default function Dashboard() {
             <MonthlyBalanceTrend />
           </Col>
           <Col xs={24} sm={12} lg={12}>
-            <CategoryBreakdown data={categoryData} />
+            <CategoryBreakdown />
           </Col>
         </Row>
       </div>
